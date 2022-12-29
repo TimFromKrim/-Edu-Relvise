7c60c57cc856663b0fa74f6a0f5dec6af483df96
Tim Djalagonia <norven15@yandex.ru>
2022-12-30 01:15:56 +0400

    c0.2
    Header final! Add blur effect, noScroll, scroll effect and minimize on mobile

--- js/header.js
+++ js/header.js
@@ -1,10 +1,23 @@
 const burgerButton = document.querySelector("#burger-button");
 const popup = document.querySelector("#popup");
-console.log("Ok!");
+const body = document.body;
+const header = document.querySelector("#header");
 
 burgerButton.addEventListener("click", burgerHandler);
 
 function burgerHandler(e) {
 	e.preventDefault();
 	popup.classList.toggle("popup-open");
+	burgerButton.classList.toggle("burger-button-active");
+	body.classList.toggle('noScroll');
 }
+
+window.addEventListener('scroll', () => {
+	if (window.scrollY > 0) {
+	  header.classList.add('header-scrolled');
+	} else {
+	  header.classList.remove('header-scrolled');
+	}
+ });
+
+console.log("Ok!");
